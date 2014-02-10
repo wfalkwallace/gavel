820ddbbe9f0cf656db9efc7db0157c1ee4ea7d26
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-10 02:29:47 -0500

    regenesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gavel::Application.config.secret_key_base = '27eac8b62288c58a169d77a6a152875ea9320ad6104d2d9aa3aee1ce7797eb206aed034cd5efac0589f791964325ce015a708d451f7cc10b872029f47ad4e03c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gavel::Application.config.session_store :cookie_store, key: '_gavel_session'

5e8757fce85a8da0cdddf2f0f58969c8ede384bb
William Falk-Wallace <wgf2104@columbia.edu>
2014-02-24 23:21:18 -0500

    cleanup; models work

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post
 
   has_many :comments
   has_many :responses
-  belongs_to :complainant, class_name: "Users", inverse_of: :complaint
-  belongs_to :defendant, class_name: "Users", inverse_of: :defense
+  belongs_to :complainant, class_name: "User", inverse_of: :complaint
+  belongs_to :defendant, class_name: "User", inverse_of: :defense
 
   field :complaint, type: String
   field :defense, type: String

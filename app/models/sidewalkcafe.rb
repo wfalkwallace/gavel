9c017b57635db52e54d0963135c43dda36c71fbf
William Falk-Wallace <wgf2104@columbia.edu>
2014-03-03 22:48:23 -0500

    adds sidewalk cafe model

---
+++ app/models/sidewalkcafe.rb
@@ -0,0 +1,20 @@
+class SidewalkCafe
+  include Mongoid::Document
+
+  field :dca_id, type: Integer
+  field :business_name, type: String
+  field :trade_name, type: String
+  field :sidewalk_cafe_type, type: String
+  field :license_number, type: Integer
+  field :full_address, type: String
+  field :address_building, type: String
+  field :address_street_name, type: String
+  field :city, type: String
+  field :state, type: String
+  field :zip_code, type: String
+  field :borough, type: String
+  field :address_location, type: String
+  field :phone, type: String
+  field :lic_area_sqft, type: Integer
+
+end

5217fc87badab3cf7d7b73d74701ecb8748b83c8
William Falk-Wallace <wgf2104@columbia.edu>
2014-03-29 04:03:51 -0700

    cleans up posts - single user tweets

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,13 @@ class Post
   has_many :comments, as: :commentable
   has_many :responses
   belongs_to :complainant, class_name: "User", inverse_of: :complaint
-  belongs_to :defendant, class_name: "User", inverse_of: :defense
 
   field :complaint, type: String
-  field :defense, type: String
-  field :created_at, type: DateTime
-  field :updated_at, type: DateTime
+  field :created_at, type: DateTime, default: -> { Time.now }
+  field :updated_at, type: DateTime, default: -> { Time.now }
 
   validates :complainant, presence: true
-  validates :complaint, presence: true
+  validates :complaint, presence: true, length: { maximum: 150 }
   validates :created_at, presence: true
   validates :updated_at, presence: true
 end

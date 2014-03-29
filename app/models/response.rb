9c64a0083238249ddfc27a60fdd5dc6790f3f1d6
William Falk-Wallace <wgf2104@columbia.edu>
2014-03-29 04:08:12 -0700

    limit and reorg responses; defualts votes to none

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,15 +6,15 @@ class Response
   belongs_to :post
   has_many :comments, as: :commentable
 
-  field :vote_count, type: Integer
-  field :body, type: String
-  field :verdict, type: Integer
+  # TODO: check this defualt syntax
+  field :vote_count, type: Integer, default: -> { 0 }
+  field :verdict, type: String
   field :created_at, type: DateTime, default: -> { Time.now }
   field :updated_at, type: DateTime, default: -> { Time.now }
 
   validates :author, presence: true
   validates :post, presence: true
-  validates :body, presence: true
+  validates :verdict, presence: true, length: { maximum: 50 }
   validates :created_at, presence: true
   validates :updated_at, presence: true
 end

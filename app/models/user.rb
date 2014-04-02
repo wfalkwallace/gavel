8f8eeaf46a3d5e1965ebe8673e3d824893d7ae2d
William Falk-Wallace <wgf2104@columbia.edu>
2014-04-01 20:30:37 -0400

    remove defense

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User
   include Mongoid::Document
 
-  has_many :comments, inverse_of: :author
   has_many :responses, inverse_of: :author
   has_many :complaints, class_name: "Post", inverse_of: :complainant
-  has_many :defenses, class_name: "Post", inverse_of: :defendant
   has_and_belongs_to_many :votes, class_name: "Response", inverse_of: :voter
 
 

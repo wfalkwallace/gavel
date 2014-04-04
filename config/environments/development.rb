28c1a9d64fd43d248d77bee021cf94562207587d
William Falk-Wallace <wgf2104@columbia.edu>
2014-04-03 23:07:47 -0400

    devise fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,7 @@ Gavel::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Devise setup
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
